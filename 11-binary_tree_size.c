2b5bf9756bbda315e6f0d579e22c0ce0416b25ae
badreaz <baz.23.2019@gmail.com>
2023-10-30 20:55:56 +0400

    create 11-binary_tree_size.c

---
+++ 11-binary_tree_size.c
@@ -0,0 +1,20 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_size - measure the size of a binary tree
+ * @trre: tree to measure size of.
+ *
+ * Return: size of the tree, otherwise 0.
+ */
+size_t binary_tree_size(count binary_tree_t *tree)
+{
+	size_t ans = 0;
+
+	if (tree)
+	{
+		ans += 1;
+		ans += binary_tree_size(tree->left);
+		ans += binary_tree_size(tree->right);
+	}
+	return (ans);
+}
