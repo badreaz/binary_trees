a1d98bdbcb9725ae720709fac57f4a5fe5845f48
badreaz <baz.23.2019@gmail.com>
2023-10-31 12:36:56 +0400

    add function binary_tree_is_leaf

--- 15-binary_tree_is_full.c
+++ 15-binary_tree_is_full.c
@@ -19,3 +19,16 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	}
 	return (ans);
 }
+
+/**
+ * binary_tree_is_leaf - checks if a noe is a leaf
+ * @node: node to check.
+ *
+ * Return: 1 if node is leaf, otherwise 0.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node && !node->left && !node->right)
+		return (1);
+	return (0);
+}
