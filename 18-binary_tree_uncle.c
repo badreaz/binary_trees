69d1301f12e9d1191fba1cea9760cbb0c879607b
badreaz <baz.23.2019@gmail.com>
2023-10-31 12:18:33 +0400

    create 18-binary_tree_uncle.c

---
+++ 18-binary_tree_uncle.c
@@ -0,0 +1,23 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_unclec-cfinds the uncle of a node
+ * @node: node to finde the uncle.
+ *
+ * Return: uncle node, otherwise NULL.
+ */
+binary_tree_t *binary_tree_uncle(binary_tree_t *node)
+{
+	binary_tree_t *parent;
+
+	if (node)
+	{
+		if (node->parent)
+			parent = node->parent->parent;
+		if (parent && parent->left == node->parent)
+			return (parent->right);
+		else if (parent)
+			return (parent->left);
+	}
+	return (NULL);
+}
