152ecbe894220f804d6ce85f50c72e33a359c897
badreaz <baz.23.2019@gmail.com>
2023-10-31 12:35:13 +0400

    add function binary_tree_is_leaf

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -1,4 +1,4 @@
-#include "binary_tree.h"
+#include "binary_trees.h"
 
 /**
  * binary_tree_nodes - counts the nodes with at least 1 child
@@ -18,4 +18,17 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 		ans += binary_tree_nodes(tree->right);
 	}
 	return (ans);
+}
+
+/**
+ * binary_tree_is_leaf - checks if a noe is a leaf
+ * @node: node to check.
+ *
+ * Return: 1 if node is leaf, otherwise 0.
+ */
+int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node && !node->left && !node->right)
+		return (1);
+	return (0);
 }
