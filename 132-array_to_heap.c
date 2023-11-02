b960033efe2fd66a21ba0d46f0ec4920f9019571
badreaz <baz.23.2019@gmail.com>
2023-11-03 01:00:59 +0400

    create 132-array_to_heap.c

---
+++ 132-array_to_heap.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * array_to_heap - builds a max binary heap tree from an array
+ * @array: pointer to array.
+ * @size: array size.
+ *
+ * Return: root node, otherwise NULL.
+ */
+heap_t *array_to_heap(int *array, size_t size)
+{
+	array++;
+	size++;
+	return (NULL);
+}
