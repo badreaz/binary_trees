be6be4a1702e83194135f92e604098698e04c541
badreaz <baz.23.2019@gmail.com>
2023-11-03 01:20:58 +0400

    create 134-heap_to_sorted_array.c

---
+++ 134-heap_to_sorted_array.c
@@ -0,0 +1,16 @@
+#include "binary_trees.c"
+
+/**
+ * heap_to_sorted_array - converts a binary max heap
+ *  to a sorted array of integers
+ * #heap: pointer to the root.
+ * @size: address to store the size of the array.
+ *
+ * Return: array is desceending order.
+ */
+int *heap_to_sorted_array(heap_t *heap, size_t *size)
+{
+	heap[0] = 0;
+	size++;
+	return (NULL);
+}
