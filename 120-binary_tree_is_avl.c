ba58b62740de0744d36205ed3a9b6938a32164af
badreaz <baz.23.2019@gmail.com>
2023-11-02 22:40:26 +0400

    create 120-binary_tree_is_Avl.c

---
+++ 120-binary_tree_is_avl.c
@@ -0,0 +1,14 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_avl - checks if a binary tree is a valid AVL tree
+ * @tree: pointer to the root.
+ *
+ * Return: 1 if tree is valid, otherwise 0.
+ */
+int binary_tree_is_avl(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+	return (1);
+}
