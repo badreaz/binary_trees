b5754d419cad29d3de01bf14fd4607cbf3cbe5cd
badreaz <baz.23.2019@gmail.com>
2023-11-03 00:53:45 +0400

    create 131-heap_insert.c

---
+++ 131-heap_insert.c
@@ -0,0 +1,15 @@
+#include "binary_trees.h"
+
+/**
+ * heap_insert - inserts a value in max binary heap
+ * @root: double pointer to the root.
+ * @value: valut to store in the node.
+ *
+ * Return: created node, otherwise NULL.
+ */
+heap_t *heap_insert(heap_t **root, int value)
+{
+	*root++;
+	value++;
+	return (NULL);
+}
