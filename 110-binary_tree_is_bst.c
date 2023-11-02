b5bb794c353aa11927128bb254ddc2225307d6b1
badreaz <baz.23.2019@gmail.com>
2023-11-02 19:55:01 +0400

    create 110-binary_tree_is_bst.c

---
+++ 110-binary_tree_is_bst.c
@@ -0,0 +1,21 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_is_bst - checks if a binary tree is a valid
+ *  binary search tree
+ * @tree: tree to check.
+ *
+ * Return 1 if tree is bst, otherwise 0.
+ */
+int binary_tree_is_bst(const binary_tree_t *tree)
+{
+	if (tree && tree->parent)
+	{
+		if (!binry_tree_is_bst(tree->left))
+			return (0);
+		if (tree->n <= tree->parent->n)
+			return (0);
+		return (binary_tree_is_bst(tree->right));
+	}
+	return (0);
+}
