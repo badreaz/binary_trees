87a87d3b9634ed320315c7383d5fc53874724ef9
badreaz <baz.23.2019@gmail.com>
2023-11-03 01:14:23 +0400

    create 133-heap_extract.c

---
+++ 133-heap_extract.c
@@ -0,0 +1,13 @@
+#include "binary_trees.h"
+
+/**
+ * heap_extract - extracts the root node of a max binary heap
+ * @root: double pointer to the root.
+ * 
+ * Return: value in the root.
+ */
+int heap_extract(heap_t **root)
+{
+	root[0] = 0;
+	return (0);
+}
