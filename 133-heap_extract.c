6fc5c199415f021eb9d38f3c6ed5abd2544c8fc8
badreaz <baz.23.2019@gmail.com>
2023-11-03 01:15:57 +0400

    edit style

--- 133-heap_extract.c
+++ 133-heap_extract.c
@@ -3,7 +3,7 @@
 /**
  * heap_extract - extracts the root node of a max binary heap
  * @root: double pointer to the root.
- * 
+ *
  * Return: value in the root.
  */
 int heap_extract(heap_t **root)
